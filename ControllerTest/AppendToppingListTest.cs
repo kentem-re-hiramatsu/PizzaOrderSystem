7fadcf4fdbf37e1dac3666cba69f3e63e1eec7f3
re-hiramatsu <re-hiramatsu>
2024-05-31 11:13:36 +0900

    AppendToppingListTest実装

---
+++ ControllerTest/AppendToppingListTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzaOrderSystem.Controller;
+using PizzaOrderSystem.Model.Pizza;
+
+namespace ControllerTest
+{
+    [TestClass]
+    public class AppendToppingListTest
+    {
+        PizzaOrderManagement pizzaorderMana = new PizzaOrderManagement();
+        //appendToppingList
+        [TestMethod]
+        public void AddToppingListTest()
+        {
+            var listCount = pizzaorderMana.GetToppingOrderListCount();
+            Assert.AreEqual(listCount, 0);
+
+            pizzaorderMana.AddToppingOrderList(new BambinoPizza());
+            pizzaorderMana.AddToppingOrderList(new MargheritaPizza());
+
+            listCount = pizzaorderMana.GetToppingOrderListCount();
+            Assert.AreEqual(listCount, 2);
+        }
+
+        [TestMethod]
+        public void RemovePizzaOrderListTest()
+        {
+            pizzaorderMana.AddToppingOrderList(new BambinoPizza());
+            pizzaorderMana.AddToppingOrderList(new MargheritaPizza());
+
+            var listCount = pizzaorderMana.GetToppingOrderListCount();
+            Assert.AreEqual(listCount, 2);
+
+            pizzaorderMana.RemoveToppingOrderList(1);
+            listCount = pizzaorderMana.GetToppingOrderListCount();
+            Assert.AreEqual(listCount, 1);
+        }
+    }
+}
--- PizzaOrderSystem/Controller/PizzaOrderManagement.cs
+++ PizzaOrderSystem/Controller/PizzaOrderManagement.cs
@@ -6,12 +6,13 @@ namespace PizzaOrderSystem.Controller
     public class PizzaOrderManagement
     {
         private List<IMenuItem> _pizzaOrderList = new List<IMenuItem>();
-        private List<IMenuItem> _appendToppingList = new List<IMenuItem>();
+        private List<IMenuItem> _toppingOrderList = new List<IMenuItem>();
 
         public PizzaOrderManagement()
         {
         }
 
+        //PizzaOrderList
         public void AddPizzaOrderList(IMenuItem pizzaMenu)
         {
             _pizzaOrderList.Add(pizzaMenu);
@@ -30,6 +31,27 @@ namespace PizzaOrderSystem.Controller
         public void RemovePizzaOrderList(int index)
         {
             _pizzaOrderList.RemoveAt(index);
+        }
+
+        //ToppingOrderList
+        public void AddToppingOrderList(IMenuItem pizzaMenu)
+        {
+            _toppingOrderList.Add(pizzaMenu);
+        }
+
+        public IMenuItem GetToppingOrderList(int index)
+        {
+            return _toppingOrderList[index];
+        }
+
+        public int GetToppingOrderListCount()
+        {
+            return _toppingOrderList.Count;
+        }
+
+        public void RemoveToppingOrderList(int index)
+        {
+            _toppingOrderList.RemoveAt(index);
         }
     }
 }
