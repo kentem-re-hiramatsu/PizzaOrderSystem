d93448ea6e3f92abdc7b0697f12ac839936590e1
re-hiramatsu <re-hiramatsu>
2024-05-31 11:05:48 +0900

    PizzaOrderListTest実装

---
+++ ControllerTest/PizzaOrderListTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzaOrderSystem.Controller;
+using PizzaOrderSystem.Model.Pizza;
+
+namespace ControllerTest
+{
+    [TestClass]
+    public class PizzaOrderListTest
+    {
+        PizzaOrderManagement pizzaorderMana = new PizzaOrderManagement();
+
+        [TestMethod]
+        public void AddPizzaOrderListTest()
+        {
+            var listCount = pizzaorderMana.GetPizzaOrderListCount();
+            Assert.AreEqual(listCount, 0);
+
+            pizzaorderMana.AddPizzaOrderList(new BambinoPizza());
+            pizzaorderMana.AddPizzaOrderList(new MargheritaPizza());
+
+            listCount = pizzaorderMana.GetPizzaOrderListCount();
+            Assert.AreEqual(listCount, 2);
+        }
+
+        [TestMethod]
+        public void RemovePizzaOrderListTest()
+        {
+            pizzaorderMana.AddPizzaOrderList(new BambinoPizza());
+            pizzaorderMana.AddPizzaOrderList(new MargheritaPizza());
+
+            var listCount = pizzaorderMana.GetPizzaOrderListCount();
+            Assert.AreEqual(listCount, 2);
+
+            pizzaorderMana.RemovePizzaOrderList(1);
+            listCount = pizzaorderMana.GetPizzaOrderListCount();
+            Assert.AreEqual(listCount, 1);
+        }
+    }
+}
---
+++ PizzaOrderSystem/Controller/PizzaOrderManagement.cs
@@ -0,0 +1,35 @@
+﻿using PizzaOrderSystem.Model.Pizza;
+using System.Collections.Generic;
+
+namespace PizzaOrderSystem.Controller
+{
+    public class PizzaOrderManagement
+    {
+        private List<IMenuItem> _pizzaOrderList = new List<IMenuItem>();
+        private List<IMenuItem> _appendToppingList = new List<IMenuItem>();
+
+        public PizzaOrderManagement()
+        {
+        }
+
+        public void AddPizzaOrderList(IMenuItem pizzaMenu)
+        {
+            _pizzaOrderList.Add(pizzaMenu);
+        }
+
+        public IMenuItem GetPizzaOrder(int index)
+        {
+            return _pizzaOrderList[index];
+        }
+
+        public int GetPizzaOrderListCount()
+        {
+            return _pizzaOrderList.Count;
+        }
+
+        public void RemovePizzaOrderList(int index)
+        {
+            _pizzaOrderList.RemoveAt(index);
+        }
+    }
+}
